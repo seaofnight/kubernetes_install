aa58d441fb94176d1af0a22683a2b1148b8cd113
seaofnight <seaofnight00@gmail.com>
2019-06-19 14:49:23 +0900

    ceph shared file add

--- 04.example/02.mariadb/03.mariadb_helm/02.1.crate_table.sh
+++ 04.example/02.mariadb/03.mariadb_helm/02.1.crate_table.sh
@@ -7,12 +7,5 @@ kubectl run mariadb-client --rm -it --restart='Never' \
   --namespace default \
   --command \
     -- mysql -h mysql -h mariadb.default.svc.cluster.local -uroot -p${PASSWD} \
-    -e "INSERT INTO my_database.test VALUES (1, '11');"
-
-kubectl run mariadb-client --rm -it --restart='Never' \
-  --image  docker.io/bitnami/mariadb:10.3.16-debian-9-r0 \
-  --namespace default \
-  --command \
-  -- mysql -h mariadb-slave.default.svc.cluster.local -uroot -p${PASSWD} \
-  -e "select * from my_database.test"
+    -e "CREATE TABLE my_database.test (id int not null, val varchar(255) not null);"
 
